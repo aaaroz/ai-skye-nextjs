cbad0e6762087c4117c5825912dc714e24f2acce
Muhamad Rhamdhan Mardiansyah <ram.ardiansyah18@gmail.com>
2024-10-01 18:07:26 +0700

    feat : wrapper verify module with suspense

--- app/auth/verify/page.tsx
+++ app/auth/verify/page.tsx
@@ -1,8 +1,21 @@
 import * as React from "react";
 import { VerifyPageModule } from "./_modules";
+import { LoaderIcon } from "lucide-react";
 
 const VerifyPage: React.FC = (): React.ReactElement => {
-  return <VerifyPageModule />;
+  return (
+    <React.Suspense
+      fallback={
+        <div className="py-16 w-full text-center">
+          <span className="flex">
+            <LoaderIcon className="size-6 animate-spin" /> Loading...
+          </span>
+        </div>
+      }
+    >
+      <VerifyPageModule />
+    </React.Suspense>
+  );
 };
 
 export default VerifyPage;
