aecb979fa3c481c18c6a6628720e86d4137fefaf
Muhamad Rhamdhan Mardiansyah <ram.ardiansyah18@gmail.com>
2024-10-04 19:14:41 +0700

    feat : add runtime edge on Features AI Page

--- app/apps/user/features/[id]/page.tsx
+++ app/apps/user/features/[id]/page.tsx
@@ -3,11 +3,13 @@ import { NextPage } from "next";
 import { FeaturesAIPageModule } from "./_modules";
 import { dummyFeatures } from "@/libs/entities";
 
+export const runtime = "edge";
+
 const FeaturesAIPage: NextPage<{
   params: { id: string };
 }> = ({ params: { id } }): React.ReactElement => {
   const dummyFeature = dummyFeatures.find((item) => item.id === id);
-  return <FeaturesAIPageModule data={dummyFeature}/>;
+  return <FeaturesAIPageModule data={dummyFeature} />;
 };
 
 export default FeaturesAIPage;
