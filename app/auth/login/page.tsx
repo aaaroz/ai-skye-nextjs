74ad88a2ee1877983f8b58e8e3f5ba92dce54d29
Muhamad Rhamdhan Mardiansyah <ram.ardiansyah18@gmail.com>
2024-10-12 14:13:27 +0700

    remove runtime edge at login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import { LoginPageModule } from "./_modules";
 
-export const runtime = "edge";
-
 const LoginPage: React.FC = (): React.ReactElement => {
   return <LoginPageModule />;
 };
