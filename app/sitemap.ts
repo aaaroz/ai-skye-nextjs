7241ba0fb97223368cc454f9a9a129367096a872
Muhamad Rhamdhan Mardiansyah <ram.ardiansyah18@gmail.com>
2024-10-17 12:50:48 +0700

    chore : change sitemap base url to urlProd

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,13 @@
 import { MetadataRoute } from "next";
 import { getAllFeatures } from "@/libs/actions";
-import { baseUrl } from "@/libs/entities";
 
 const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
   const response = await getAllFeatures();
+  const urlProd = "https://kontenkilat.id";
 
   const featuresRoute = response.map(({ slug }) => {
     return {
-      url: `${baseUrl}/features/${slug}`,
+      url: `${urlProd}/features/${slug}`,
       lastModified: new Date(),
       changeFrequency: "monthly" as const,
       priority: 0.8,
@@ -16,13 +16,13 @@ const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
 
   return [
     {
-      url: baseUrl,
+      url: urlProd,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: `${baseUrl}/features`,
+      url: `${urlProd}/features`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
